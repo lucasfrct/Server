bdf8f8fc259f5443b0fcc9a8eedfbd64fe2822d7
lucasfrct <lucasfrct@gmail.com>
2023-07-20 14:03:25 -0300

    update

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "github.com/lucasfrct/servertools/pkg/modules/proxy"
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/lucasfrct/servertools/pkg/modules/schedule"
+)
 
 func main() {
-	proxy.Reverse()
+	// proxy.Reverse()
+	// 	schedule.Schedule()
+	done := schedule.Schedule(func() {
+		println("Schedule: \n")
+
+		command := ""
+		cmdarr := strings.Split(command, " ")
+		cmd, err := exec.Command(cmdarr[0], cmdarr[1:]...).Output()
+		if err != nil {
+			fmt.Printf("Erro: %v \n", err)
+		}
+
+		fmt.Printf("%s \n", string(cmd))
+
+	}, 5*time.Second)
+
+	select {
+	case v1 := <-done:
+		fmt.Println("got: ", v1)
+	}
+
+	done <- false
+
 }
