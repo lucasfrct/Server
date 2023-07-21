dbfcf7e2f98e1bf1adc80b0a2fad4e84268d6d13
lucasfrct <lucasfrct@gmail.com>
2023-07-21 12:44:30 -0300

    criando codigo para copiar arquivos

--- main.go
+++ main.go
@@ -9,28 +9,46 @@ import (
 	"github.com/lucasfrct/servertools/pkg/modules/schedule"
 )
 
+func copy(command string) error {
+
+	cmdarr := strings.Split(command, " ")
+	cmd, err := exec.Command(cmdarr[0], cmdarr[1:]...).Output()
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%s \n", string(cmd))
+	return nil
+}
+
 func main() {
 	// proxy.Reverse()
 	// 	schedule.Schedule()
-	done := schedule.Schedule(func() {
+	done := make(chan bool)
+
+	done = schedule.Schedule(func() {
 		println("Schedule: \n")
 
-		command := ""
-		cmdarr := strings.Split(command, " ")
-		cmd, err := exec.Command(cmdarr[0], cmdarr[1:]...).Output()
+		// Construa o comando xcopy
+		command := `xcopy C:\Users\lucas\Desktop\Origem\ C:\Users\lucas\Desktop\Destino\ /s /e /y`
+
+		err := copy(command)
 		if err != nil {
-			fmt.Printf("Erro: %v \n", err)
+			fmt.Print(err)
 		}
 
-		fmt.Printf("%s \n", string(cmd))
+		done <- true
 
 	}, 5*time.Second)
+
+	go func() {
+		time.Sleep(4 * time.Second)
+		done <- true
+	}()
 
 	select {
 	case v1 := <-done:
 		fmt.Println("got: ", v1)
 	}
-
-	done <- false
 
 }
